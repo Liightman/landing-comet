72e0d7276f68913fac842f8de562d80699b40fda
Llyam Garcia <lgarcia@student.42.fr>
2017-04-23 10:51:03 +0200

    Try to fix stallite and rocket in medium device

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,15 @@
 if (window.screen.availHeight <= 824)
 {
-    $('#satellite').css('top', '-291px');
-    $('#rocket').css('top', '-275px');
+    if (navigator.userAgent.indexOf("Chrome") !== -1)
+    {
+        $('#satellite').css('top', '-291px');
+        $('#rocket').css('top', '-275px');
+    }
+
+    if (navigator.userAgent.indexOf("Chrome") !== -1)
+    {
+        $('#satellite').css('margin-top', '-16.7%');
+        $('#rocket').css('margin', '-16.7%');
+    }
+
 }
