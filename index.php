d034d294fd66a865212446269907c74fe0d60405
Llyam Garcia <lgarcia@student.42.fr>
2017-04-25 22:09:46 +0200

    Edit redirection

--- index.php
+++ index.php
@@ -1,1 +1,70 @@
-<?php header( 'Location: /index' ) ;
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="" />
+    <meta name="author" content="Llyam Garcia, Comet Yolo" />
+    <meta name="keywords" content="Comet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Comet</title>
+
+    <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/helloComet-1536.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="background-landing">
+    <div class="container" style="margin-top: 3rem;">
+        <div class="col-lg-6">
+            <img src="assets/images/svg/logo.svg" alt="Comet" class="logo">
+        </div>
+    </div>
+
+    <div class="container" style="padding-bottom: 6rem">
+        <h1 id="we-are-comet" class="col-lg-8">Nous sommes Comet,
+            <br>la meilleure manière
+            <br>de travailler en tech/data
+        </h1>
+
+        <div class="col-lg-12">
+            <img src="assets/images/svg/we-are-separator.svg" alt="separator" class="we-are-comet-separator">
+        </div>
+
+        <div id="presentation" class="col-xs-12 col-sm-12 col-md-8 col-lg-6">
+            Comet est une plateforme qui connecte les meilleurs freelances tech / data avec les entreprises les plus innovantes
+        </div>
+
+        <div id="callActions" class="col-lg-8">
+            <div class="col-xs-12 col-sm-12 col-lg-5">
+                <img src="assets/images/svg/astronaut.svg" alt="Freelances" class="astronaut center">
+                <div id="freelances-text" class="center destination col-xs-12 col-sm-12 col-lg-12">Freelances</div>
+                <button id="freelancesActionButton" class="btn col-xs-12 col-sm-12 col-lg-12 center">
+                        <span class="center">
+                            Rejoindre Comet
+                            <img src="assets/images/left-arrow.png" alt="Rejoindre Comet" class="leftArrow">
+                        </span>
+                </button>
+            </div>
+            <hr class="hidden-lg col-xs-12 col-md-11">
+            <div class="col-xs-12 col-sm-12 col-lg-5">
+                <img src="assets/images/svg/planet.svg" alt="Freelances" class="planet center">
+                <div id="business-text" class="col-xs-12 destination col-sm-12 col-lg-12 center">Entreprises</div>
+                <button id="businessActionButton" class=" btn col-xs-12 col-sm-12 col-lg-12 center">
+                        <span class="center">
+                            Trouver un freelance
+                            <img src="assets/images/left-arrow.png" alt="Trouver un freelance" class="leftArrow">
+                        </span>
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="navbar bottom navbar-fixed-bottom hidden-xs hidden-sm hidden-md"></div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.js" type="text/javascript"></script>
+    <script src="assets/js/index.js" type="text/javascript"></script>
+</div>
+</body>
+</html>
