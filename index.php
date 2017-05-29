5f054dec65e61ed7483e847ddc3680b0338fac46
Llyam Garcia <lgarcia@student.42.fr>
2017-05-29 23:09:23 +0200

    Page speed improvements

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
     <html lang="fr">
         <head>
             <meta charset="UTF-8">
+
             <meta name="description" content="Comet, une solution simple et transparente pour vous faciliter la vie."/>
             <meta name="author" content="Llyam Garcia, Comet Yolo"/>
             <meta name="keywords" content="Comet"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <link rel="icon" type="image/x-icon" href="https://s3.eu-west-2.amazonaws.com/comet-assets/CO_yellow.png"
-                  sizes="16x16">
+
             <link rel="icon" type="image/x-icon" href="https://s3.eu-west-2.amazonaws.com/comet-assets/CO_yellow.png"
                   sizes="32x32">
             <link rel="shortcut icon" type="image/x-icon" href="https://s3.eu-west-2.amazonaws.com/comet-assets/CO_yellow.png">
@@ -15,16 +15,14 @@
             <title>comet</title>
             <!-- meta facebook -->
             <meta property="og:type" content="website"/>
-            <meta property="og:title" content="comet - la nouvelle manière de travailler tech/data" %>
+            <meta property="og:title" content="comet - la nouvelle manière de travailler tech/data">
             <meta property="og:description"
                   content="comet connecte les meilleurs freelances tech/data avec les entreprises les plus innovantes."/>
             <meta property="og:image" content="https://s3.eu-west-2.amazonaws.com/comet-assets/facebook_thumbnail.png"/>
             <meta property="og:url" content="https://www.hellocomet.co"/>
             <meta name="google-site-verification" content="tyyUOHqUeawLMCeyfRLjWDuO75EMRT_q1PHaeIS2FJY"/>
 
-            <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="bogus"/>
-            <link href="assets/css/helloComet-1536.css" rel="stylesheet" type="text/css" media="bogus"/>
-            <link href="assets/css/helloComet-1536-responsive.css" rel="stylesheet" type="text/css" media="bogus"/>
+            <link href="assets/css/comet.min.css" rel="stylesheet" type="text/css" />
         </head>
 
 
@@ -107,97 +105,11 @@
                     </div>
                 </div>
             </div>
-            
+
             <script
                     src="https://code.jquery.com/jquery-3.2.1.min.js"
                     integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
                     crossorigin="anonymous"></script>
-            <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
-            <script src="assets/js/index.js" type="text/javascript"></script>
-            <!-- tracking segment -->
-            <script type="text/javascript">
-                !function () {
-                    var analytics = window.analytics = window.analytics || [];
-                    if (!analytics.initialize)if (analytics.invoked) window.console && console.error && console.error("Segment snippet included twice."); else {
-                        analytics.invoked = !0;
-                        analytics.methods = ["trackSubmit", "trackClick", "trackLink", "trackForm", "pageview", "identify", "reset", "group", "track", "ready", "alias", "debug", "page", "once", "off", "on"];
-                        analytics.factory = function (t) {
-                            return function () {
-                                var e = Array.prototype.slice.call(arguments);
-                                e.unshift(t);
-                                analytics.push(e);
-                                return analytics
-                            }
-                        };
-                        for (var t = 0; t < analytics.methods.length; t++) {
-                            var e = analytics.methods[t];
-                            analytics[e] = analytics.factory(e)
-                        }
-                        analytics.load = function (t) {
-                            var e = document.createElement("script");
-                            e.type = "text/javascript";
-                            e.async = !0;
-                            e.src = ("https:" === document.location.protocol ? "https://" : "http://") + "cdn.segment.com/analytics.js/v1/" + t + "/analytics.min.js";
-                            var n = document.getElementsByTagName("script")[0];
-                            n.parentNode.insertBefore(e, n)
-                        };
-                        analytics.SNIPPET_VERSION = "4.0.0";
-                        analytics.load("7iOqS9L3PoyGRj04HhCqfZfkR7ofjIFV");
-                        analytics.page({})
-                        // { Intercom: { hideDefaultLauncher: true }}
-                    }
-                }();
-                //         Hotjar Tracking Code for http://www.hellocomet.co
-                (function (h, o, t, j, a, r) {
-                    h.hj = h.hj || function () {
-                            (h.hj.q = h.hj.q || []).push(arguments)
-                        };
-                    h._hjSettings = {hjid: 485099, hjsv: 5};
-                    a = o.getElementsByTagName('head')[0];
-                    r = o.createElement('script');
-                    r.async = 1;
-                    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-                    a.appendChild(r);
-                })(window, document, '//static.hotjar.com/c/hotjar-', '.js?sv=');
-
-                if (location.protocol !== 'https:')
-                    location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
-
-                $('.cta-button').on('mouseover', function () {
-                    $($(this).parent().find("img")[0]).addClass('floating');
-                }).on('mouseout', function () {
-                    $($(this).parent().find("img")[0]).removeClass('floating');
-                })
-
-                setTimeout(popup, 2000);
-
-                function popup() {
-                    $("#toexport").insertBefore("body").animate({opacity: 0.9}, 1500).fadeIn(2000).removeClass('hidden');
-                    $(".first-container").css("margin-top", "-1rem");
-                }
-                $(".clicks").css('top', '10rem');
-                $(document).on('click', '.close', function () {
-                    $(".clicks").css('top', '3rem');
-                })
-                $(document).ready(function () {
-                    var link_f = $('.cta-button');
-                    $.each(link_f, function (index, value) {
-                        if ($(value).attr('name').includes("Freelance")) {
-                            var libel = 'Freelance_sign_up'
-                        }
-                        else {
-                            var libel = 'Entreprise_sign_up'
-                        }
-                        analytics.trackLink($(value), 'Click_CTA', {
-                            category: libel,
-                            label: $(value).attr('name'),
-                        });
-                    });
-                })
-            </script>
-
-            <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-            <link href="assets/css/helloComet-1536.css" rel="stylesheet" type="text/css"/>
-            <link href="assets/css/helloComet-1536-responsive.css" rel="stylesheet" type="text/css"/>
+            <script src="assets/js/comet.min.js" type="text/javascript"></script>
         </body>
     </html>
