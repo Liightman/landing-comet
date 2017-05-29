64a887bb4bfefe744dda1ea596d881a956d14802
Llyam Garcia <lgarcia@student.42.fr>
2017-05-29 23:10:40 +0200

    Page speed improvements

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
             <meta property="og:url" content="https://www.hellocomet.co"/>
             <meta name="google-site-verification" content="tyyUOHqUeawLMCeyfRLjWDuO75EMRT_q1PHaeIS2FJY"/>
 
+            <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
             <link href="assets/css/comet.min.css" rel="stylesheet" type="text/css" />
         </head>
 
